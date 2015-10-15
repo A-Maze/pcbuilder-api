492ca397bcbdfe43fbecf9e5c70f57c42c225c22
Robin <RobinSiep1@gmail.com>
2015-10-15 20:33:29 +0200

    default routing and view

--- api/api/handlers/home.py
+++ api/api/handlers/home.py
@@ -1,2 +1,6 @@
-def my_view(request):
-    return {'project':'api'}
+from pyramid.view import view_config
+
+
+@view_config(route_name='home', renderer='json')
+def root_view(request):
+    return {"api": 0.1}
