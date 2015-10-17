372e63a428d0fa1b8fda4c53859b2ca065d3258d
Robin Siep <RobinSiep1@gmail.com>
2015-10-17 16:08:18 +0200

    working mongodb shizzle hopefully

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,7 +1,4 @@
 from pyramid.config import Configurator
-from pyramid.events import subscriber
-from pyramid.events import NewRequest
-import pymongo
 
 from api.lib.factories.root import RootFactory
 
@@ -10,23 +7,8 @@ def main(global_config, **settings):
     """ This function returns a WSGI application.
     """
     config = Configurator(settings=settings, root_factory=RootFactory)
+    config.include("pyramid_mongoengine")
+    config.add_connection_database()
     config.add_route('home', '/')
-    # MongoDB
-
-    def add_mongo_db(event):
-        settings = event.request.registry.settings
-        url = settings['mongodb.url']
-        db_name = settings['mongodb.name']
-        db = settings['mongodb_conn'][db_name]
-        event.request.db = db
-    db_uri = settings['mongodb.url']
-    MongoDB = pymongo.MongoClient
-    if 'pyramid_debugtoolbar' in set(settings.values()):
-        class MongoDB(pymongo.Connection):
-            def __html__(self):
-                return 'MongoDB: <b>{}></b>'.format(self)
-    conn = MongoDB(db_uri)
-    config.registry.settings['mongodb_conn'] = conn
-    config.add_subscriber(add_mongo_db, NewRequest)
     config.scan('api.handlers')
     return config.make_wsgi_app()
