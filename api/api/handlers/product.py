bea6e211bba455d1185b305bff4e7b8936a4a8c4
Robin <RobinSiep1@gmail.com>
2015-10-26 11:48:15 +0100

    forgot to push a file

--- api/api/handlers/product.py
+++ api/api/handlers/product.py
@@ -2,18 +2,35 @@ import logging
 import json
 from bson.json_util import dumps
 from pyramid.view import view_config
+from api.models.category import Category
 from functools import partial
 
 log = logging.getLogger(__name__)
 
-product_view = partial(
+products_view = partial(
     view_config,
     permission='public',
     renderer='json')
 
+product_view = partial(
+    view_config,
+    permission='public',
+    renderer='json',
+    context=Category,
+    name='product'
+    )
 
-@product_view(request_method="GET")
-def return_product(request):
+
+@products_view(request_method="GET")
+def return_products(request):
     products = request.context
     products_dict = [json.loads(dumps(obj.to_mongo())) for obj in products]
     return {"products": products_dict}
+
+
+@product_view(request_method="GET")
+def return_product(request):
+    product_id = request.subpath[0]
+    product = request.context.get_product(product_id)
+    product_json = json.loads(dumps(product.to_mongo()))
+    return {'product':product_json}
