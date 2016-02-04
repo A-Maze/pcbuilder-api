ac77b8db1dc80c6a417ff3e183c10da8771c8ffa
Robin Siep <RobinSiep1@gmail.com>
2016-02-04 22:16:55 +0100

    version bump 1.1

--- api/api/handlers/home.py
+++ api/api/handlers/home.py
@@ -4,4 +4,4 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='json',
                         request_method='GET')
 def root_view(request):
-    return {"api": 1.0}
+    return {"api": 1.1}
